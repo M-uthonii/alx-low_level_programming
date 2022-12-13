421fdc1f28771982126a034e4ab64655c84aefba
M-uthonii <muthoni2002m@gmail.com>
2022-12-13 13:24:53 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - entry
+ * Return - Always 0
+ */
+int main(void)
+{
+	char low
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
