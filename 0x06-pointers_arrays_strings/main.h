5cf206af295135365fb3f15c371d2c55e66eaa30
M-uthonii <muthoni2002m@gmail.com>
2022-12-22 08:14:37 -0800

    correction

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,6 +13,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int check_seperators(char c)
+int check_seperators(char c);
 
 #endif
